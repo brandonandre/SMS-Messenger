dbc215a758b11500c44f626d9f3bb4f24db16e7e
Brandon Andre <dev.brandonandre@gmail.com>
2017-03-28 21:29:40 -0400

    Added Inbox Activity

---
+++ app/src/main/java/com/messenger/brandon/messenger/InboxActivity.java
@@ -0,0 +1,13 @@
+package com.messenger.brandon.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InboxActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox);
+    }
+}
